e06261b1e1aeac872990c5203abae1600832aa0d
juanodicio <juanodicio@f9abf120-7ea8-11de-8c42-8d293cda74e2>
2009-08-11 21:36:20 +0000

    - Webmail example

---
+++ examples/webmail/config.php
@@ -0,0 +1,13 @@
+<?php
+include_once '../../Odimail/Connection.php';
+include_once '../../Odimail/Contact.php';
+include_once '../../Odimail/Message/Part.php';
+include_once '../../Odimail/Message.php';
+
+$config = array(
+    'user' => 'dexter',
+    'password' => '123456',
+    'host' => 'mail.one-src.com',
+    'mailbox' => 'INBOX',
+    'flags' => array('notls')
+);
